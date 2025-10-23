1abf57b1296cda55970a60c0775ab3b7cb321773
LuisFilipeVS <lfilipevs.contato@gmail.com>
2025-10-23 20:59:43 -0300

    Atualizacao da funcionalidade do bot

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import telebot
 import os
 from dotenv import load_dotenv
+from telethon import TelegramClient, events
 
 load_dotenv()
 
-API_TOKEN = os.getenv("TELEGRAM_TOKEN")
-bot = telebot.TeleBot(API_TOKEN)
+API_TOKEN = os.getenv("BOT_TOKEN")
 
+SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL"))
+TARGET_CHANNEL = int(os.getenv("TARGET_CHANNEL"))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+bot = telebot.TeleBot(API_TOKEN)
+notification_bot = telebot.TeleBot(API_TOKEN)
+
+KEYWORDS = ["Placa de Vídeo", "Smartphone", "Notebook", "Monitor", "Tablet", "Smart TV", "Câmera", "Fone de Ouvido", "Console de Videogame"]
+
+client = TelegramClient('promo_monitor', API_ID, API_HASH)
+
+@client.on(events.NewMessage(chats=SOURCE_CHANNEL))
+async def handler_new_message(event):
+    message = event.message.message
+
+    if any(keyword.lower() in message.lower() for keyword in KEYWORDS):
+        notification_text = f"🔥 **PROMOÇÃO ENCONTRADA!** 🔥\n\n{message}"
+
+        #notification_bot.send_message(TARGET_CHANNEL, notification_text)
+        
+        try:
+            await client.send_message(TARGET_CHANNEL, notification_text)
+            print("Promoção enviada para o canal de destino.")
+        except Exception as e:
+            print(f"Erro ao enviar mensagem para o canal de destino: {e}")
+
+
+def start_bot():
+    with client:
+        print("Bot de monitoramento iniciado.")
+        client.run_until_disconnected()
+    
 def verify_commands(message=""):
     return True
 
@@ -57,4 +90,9 @@ def send_welcome(message):
     bot.send_message(message.chat.id, text)
 
 # Start polling
-bot.polling()
+# bot.polling()
+
+if __name__ == "__main__":
+    print("Iniciando o bot de monitoramento de promoções...")
+    start_bot()
+
